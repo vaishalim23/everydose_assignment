8bc066b0da9b29449c36197cfa348aab97d1d168
Vaishali M <vaishalim@ideyalabs.com>
2023-01-12 20:43:42 +0530

    changed file

--- src/Components/EditList/EditList.js
+++ src/Components/EditList/EditList.js
@@ -3,7 +3,7 @@ import styles from './EditList.module.scss'
 export function EditList({clickEditHandler}){
     return(
         <div>
-            <button className={styles.edit_button} onClick={() => clickEditHandler(false)}>Edit List</button>
+            <button className={styles.edit_button} onClick={() => clickEditHandler(false)}>Edit list</button>
         </div>
     )
 }
